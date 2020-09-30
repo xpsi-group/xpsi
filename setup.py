e49aa30aa2dcf0daa614b734edd01c1880c039cf
Thomas E. Riley <T.E.Riley@uva.nl>
2020-09-30 20:31:38 +0200

    bump patch number

--- setup.py
+++ setup.py
@@ -198,7 +198,7 @@ if __name__ == '__main__':
 
     setup(
         name = 'xpsi',
-        version = '0.6.1',
+        version = '0.6.2',
         author = 'Thomas Edward Riley',
         author_email = 't.e.riley@uva.nl; t.riley.phd@gmail.com',
         url = 'https://github.com/ThomasEdwardRiley/xpsi',
