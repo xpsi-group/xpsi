df0fb02f367c8aa9dc133a13e10d0a722c22236b
Daniela Huppenkothen <d.huppenkothen@sron.nl>
2022-09-19 17:35:41 +0200

    Added tests for init of spacetime

--- xpsi/tests/test_spacetime.py
+++ xpsi/tests/test_spacetime.py
@@ -1,10 +1,11 @@
 from __future__ import division, print_function
 
-from .Spacetime import Spacetime
+from xpsi.Spacetime import Spacetime
+import pytest
 
-def TestSpacetime(object):
+class TestSpacetime(object):
 
-    def setup_class(self):
+    def setup_class(cls):
         cls.frequency = 300.0
         cls.mass = 1.4
         cls.radius = 10.0
@@ -17,54 +18,61 @@ def TestSpacetime(object):
         cls.distance_bounds = (0.05, 25.0)
         cls.cos_inclination_bounds = (-0.9, 0.9)
 
-        cls.bounds = (self.frequency_bounds,
-                      self.mass_bounds,
-                      self.radius_bounds,
-                      self.distance_bounds,
-                      self.cos_inclination_bounds)
+        cls.bounds = {"frequency": cls.frequency_bounds,
+                      "mass": cls.mass_bounds,
+                      "radius": cls.radius_bounds,
+                      "distance": cls.distance_bounds,
+                      "cos_inclination": cls.cos_inclination_bounds}
 
-        cls.values = (self.frequency,
-                      self.mass,
-                      self.radius,
-                      self.distance,
-                      self.cos_inclination)
+        cls.values = {"frequency": cls.frequency,
+                      "mass": cls.mass,
+                      "radius": cls.radius,
+                      "distance": cls.distance,
+                      "cos_inclination": cls.cos_inclination}
 
     def test_spacetime_class_initializes(self):
         st = Spacetime(self.bounds, self.values)
 
     def test_spacetime_breaks_with_values_missin(self):
-        st = Spacetime(self.values)
+        with pytest.raises(TypeError):
+            st = Spacetime(self.values)
 
     def test_spacetime_breaks_with_bounds_missing(self):
-        st = Spacetime(self.bounds)
+        with pytest.raises(TypeError):
+            st = Spacetime(self.bounds)
 
     def test_spacetime_breaks_with_both_missing(self):
-        st = Spacetime()
+        with pytest.raises(TypeError):
+            st = Spacetime()
 
     def test_spacetime_breaks_if_inputs_switched(self):
-       st = Spacetime(self.values, self.bounds)
+       with pytest.raises(TypeError):
+           st = Spacetime(self.values, self.bounds)
 
     def test_failure_when_parameter_out_of_bounds(self):
         mass = 3.5
-        values = (self.frequency, mass, self.radius, self.distance,
-                  self.cos_inclination)
-
-        st = Spacetime(values, self.bounds)
+        values = {"frequency": self.frequency, 
+                  "mass": mass, 
+                  "radius": self.radius, 
+                  "distance": self.distance,
+                  "cos_inclination": self.cos_inclination}
+        with pytest.raises(TypeError):
+            st = Spacetime(values, self.bounds)
 
     def test_failure_when_parameter_out_of_strict_bounds(self):
         mass_bounds = (0.001, 3.5)
-        bounds = (self.frequency_bounds,
-                  mass_bounds,
-                  self.radius_bounds,
-                  self.distance_bounds,
+        bounds = {"frequency": self.frequency_bounds,
+                  "mass": mass_bounds,
+                  "radius": self.radius_bounds,
+                  "distance": self.distance_bounds,
+                  "cos_inclination": self.cos_inclination_bounds}
+        with pytest.raises(ValueError):
+            st = Spacetime(bounds, self.values)
+
+    def test_fails_when_input_is_not_dict(self):
+        bounds = (self.frequency_bounds, self.mass_bounds,
+                  self.radius_bounds, self.distance_bounds,
                   self.cos_inclination_bounds)
 
-        st = Spacetime(bounds, self.values)
-
-    def test_failure_when_input_none(self):
-        mass = None
-        values = (self.frequency, mass, self.radius, self.distance, 
-                  self.cos_inclination)
-        st = Spacetime(self.bounds, self.values)
-
-
+        with pytest.raises(TypeError):
+            st = Spacetime(bounds, self.values)
