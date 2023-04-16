b3a14ff4d074ab1ea3d1e8ecb67161ea208bcc69
Tuomo Salmi <tuomosalmi1@gmail.com>
2023-04-16 10:26:36 +0200

    Making basic numerical test run working as well.

--- examples/examples_modeling_tutorial/TestRun_Num.py
+++ examples/examples_modeling_tutorial/TestRun_Num.py
@@ -134,6 +134,7 @@ primary = xpsi.HotRegion(bounds=bounds,
 	                    max_sqrt_num_cells=64,
 	                    num_leaves=100,
 	                    num_rays=200,
+	                    atm_ext=2,
 	                    prefix='p')
 
 class derive(xpsi.Derive):
@@ -170,6 +171,7 @@ secondary = xpsi.HotRegion(bounds=bounds, # can otherwise use same bounds
 	                      num_rays=200,
 	                      do_fast=False,
 	                      is_antiphased=True,
+	                      atm_ext=2,
 	                      prefix='s')
 
 
