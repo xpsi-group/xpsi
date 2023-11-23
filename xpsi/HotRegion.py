4fa83e017cba573e7207ad11cd203fb43ac4852e
Tuomo Salmi <tuomosalmi1@gmail.com>
2023-11-23 18:30:13 +0100

    Revert "Azimuthal invariant stokes integrator optimized if only want to get I."
    This reverts commit 1dd0312bc006527de05c3a3f260981db2c532b06.

--- xpsi/HotRegion.py
+++ xpsi/HotRegion.py
@@ -1244,7 +1244,6 @@ class HotRegion(ParameterSubspace):
                             integration.
 
         """
-        stokes=1 #need to think how to pass this from photosphere. self.stokes is not working, since self if hot object.
         if self.fast_mode and not self.do_fast:
             try:
                 if self.cede:
@@ -1300,7 +1299,6 @@ class HotRegion(ParameterSubspace):
                                        self.atm_ext,
                                        atm_ext_else,
                                        self.beam_opt,
-                                       stokes,
                                        self._image_order_limit)
             super_pulse = all_pulses[0], all_pulses[1] #success and flux
             super_pulse_Q = all_pulses[0], all_pulses[2]
@@ -1334,7 +1332,6 @@ class HotRegion(ParameterSubspace):
                                        self.atm_ext,
                                        atm_ext_else,
                                        self.beam_opt,
-                                       stokes,
                                        self._image_order_limit)
             super_pulse = all_pulses[0], all_pulses[1] #success and flux
             super_pulse_Q = all_pulses[0], all_pulses[2]
@@ -1374,7 +1371,6 @@ class HotRegion(ParameterSubspace):
                                        self.atm_ext,
                                        atm_ext_else,
                                        self.beam_opt,
-                                       stokes,
                                        self._image_order_limit)
             cede_pulse = all_pulses[0], all_pulses[1] #success and flux
             cede_pulse_Q = all_pulses[0], all_pulses[2]
