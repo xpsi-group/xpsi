5e2876c40cef0e127827dd6df5cc969a1a6924d5
Tuomo Salmi <tuomosalmi1@gmail.com>
2023-12-08 19:21:48 +0100

    Changelog and version update

--- setup.py
+++ setup.py
@@ -261,7 +261,7 @@ if __name__ == '__main__':
 
     setup(
         name = 'xpsi',
-        version = '2.1.1',
+        version = '2.1.2',
         author = 'The X-PSI Core Team',
         author_email = 'A.L.Watts@uva.nl',
         url = 'https://github.com/xpsi-group/xpsi',
