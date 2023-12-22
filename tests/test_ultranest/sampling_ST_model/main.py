f3dc730a06b2d91f519dee635ce24810394282de
Mariska Hoogkamer <mariska.98@hotmail.com>
2023-12-22 11:08:44 +0100

    Update default runtime params

--- tests/test_ultranest/sampling_ST_model/main.py
+++ tests/test_ultranest/sampling_ST_model/main.py
@@ -164,7 +164,7 @@ def run_sampler(likelihood, prior, directory, name, sample_number):
     sampler = xpsi.Sample.ultranested(likelihood=likelihood, 
                                         prior=prior, 
                                         sampler_params={'log_dir' : output_dir},
-                                        runtime_params={'show_status': True, 'min_num_live_points' : 1000},
+                                        runtime_params={'show_status':True, 'update_interval_volume_fraction': 0.1, 'min_num_live_points':5000},
                                         use_stepsampler=False, 
                                         # stepsampler_params={'max_nsteps' : 400}, 
                                         out_filename=output_filename)
