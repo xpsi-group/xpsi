cf0b52f84065e9682a63c851ecc779347c23f010
Tuomo Salmi <t.h.j.salmi@uva.nl>
2024-04-26 14:11:15 +0200

    The default imaging extension changed -tag v2.2.2 (#372)
    The default imaging extension has been changed from ``uniform.pyx`` to ``PDT_U.pyx`` to avoid the need for X-PSI re-installation when computing neutron star images for different nested models. The tutorials have been updated to account for this change when necessary. Some bugs in the imaging function were also fixed, and the Global surface emission -tutorial was added back to the documentation pages.

--- xpsi/surface_radiation_field/local_variables.pyx
+++ xpsi/surface_radiation_field/local_variables.pyx
@@ -3,7 +3,7 @@
 #cython: nonecheck=False
 #cython: wraparound=False
 
-from libc.math cimport sin, cos, acos, log, exp, pow, sqrt
+from libc.math cimport sin, cos, acos, log
 from libc.stdlib cimport malloc, free
 from libc.stdio cimport printf
 
@@ -14,18 +14,69 @@ cdef int ERROR = 1
 cdef int HIT = 1
 cdef int MISS = 0
 
+cdef double separation(double THETA, double PHI,
+                       double theta, double phi, double sin_theta) nogil:
+
+    cdef:
+        double sin_THETA = sin(THETA)
+        double psi = acos(cos(THETA) * cos(theta)
+                            + sin_THETA * sin_theta * cos(phi) * cos(PHI)
+                            + sin_THETA * sin_theta * sin(phi) * sin(PHI))
+    return psi
+
 cdef int HIT_or_MISS(double theta,
                      double phi,
                      double HYPERSLICE,
                      const double *const global_variables,
                      const storage *const buf) nogil:
-    # use this function to determine whether an arbitrary input ray transports
+    # Use this function to determine whether an arbitrary input ray transports
     # a FINITE quantity of radiation
-    #
-    # NB: HYPERSLICE is a phase parameter with units of radians that the
-    # time evolution of the photosphere as it undergoes bulk rotation
 
-    return HIT # independent of spacetime coordinates
+    cdef:
+        double super_colat_p = global_variables[0]
+        double super_azi_p = global_variables[1] + HYPERSLICE
+        double super_radius_p = global_variables[2]
+        double cede_colat_p = global_variables[3]
+        double cede_azi_p = global_variables[4] + HYPERSLICE
+        double cede_radius_p = global_variables[5]
+
+        double super_colat_s = global_variables[6]
+        double super_azi_s = global_variables[7] + HYPERSLICE
+        double super_radius_s = global_variables[8]
+        double cede_colat_s = global_variables[9]
+        double cede_azi_s = global_variables[10] + HYPERSLICE
+        double cede_radius_s = global_variables[11]
+
+        double super_log10_temperature_p = global_variables[12]
+        double super_log10_temperature_s = global_variables[14]
+
+        double sin_theta = sin(theta)
+
+    if separation(cede_colat_p, cede_azi_p,
+                  theta, phi, sin_theta) <= cede_radius_p:
+        if separation(super_colat_p, super_azi_p,
+                      theta, phi, sin_theta) > super_radius_p:
+            return HIT # in the primary radiating region
+        elif super_log10_temperature_p >= 0.0:
+            return HIT
+    elif super_log10_temperature_p >= 0.0:
+        if separation(super_colat_p, super_azi_p,
+                      theta, phi, sin_theta) <= super_radius_p:
+            return HIT # in the primary radiating region
+
+    if separation(cede_colat_s, cede_azi_s,
+                  theta, phi, sin_theta) <= cede_radius_s:
+        if separation(super_colat_s, super_azi_s,
+                      theta, phi, sin_theta) > super_radius_s:
+            return HIT # in the secondary radiating region
+        elif super_log10_temperature_s >= 0.0:
+            return HIT
+    elif super_log10_temperature_s >= 0.0:
+        if separation(super_colat_s, super_azi_s,
+                      theta, phi, sin_theta) <= super_radius_s:
+            return HIT # in the primary radiating region
+
+    return MISS # both hot regions
 
 #----------------------------------------------------------------------->>>
 # >>> User modifiable functions.
@@ -75,7 +126,51 @@ cdef int eval_local_variables(double theta,
 
     cdef double *local_vars = (<double**> buf.local_variables)[THREAD]
 
-    local_vars[0] = global_variables[0]
+    cdef:
+        double super_colat_p = global_variables[0]
+        double super_azi_p = global_variables[1] + HYPERSLICE
+        double super_radius_p = global_variables[2]
+        double cede_colat_p = global_variables[3]
+        double cede_azi_p = global_variables[4] + HYPERSLICE
+        double cede_radius_p = global_variables[5]
+
+        double super_colat_s = global_variables[6]
+        double super_azi_s = global_variables[7] + HYPERSLICE
+        double super_radius_s = global_variables[8]
+        double cede_colat_s = global_variables[9]
+        double cede_azi_s = global_variables[10] + HYPERSLICE
+        double cede_radius_s = global_variables[11]
+
+        double super_log10_temperature_p = global_variables[12]
+        double cede_log10_temperature_p = global_variables[13]
+        double super_log10_temperature_s = global_variables[14]
+        double cede_log10_temperature_s = global_variables[15]
+
+        double sin_theta = sin(theta)
+
+    if separation(cede_colat_p, cede_azi_p,
+                  theta, phi, sin_theta) <= cede_radius_p:
+        if separation(super_colat_p, super_azi_p,
+                      theta, phi, sin_theta) > super_radius_p:
+            local_vars[0] = cede_log10_temperature_p
+        elif super_log10_temperature_p >= 0.0:
+            local_vars[0] = super_log10_temperature_p
+    elif super_log10_temperature_p >= 0.0:
+        if separation(super_colat_p, super_azi_p,
+                      theta, phi, sin_theta) <= super_radius_p:
+            local_vars[0] = super_log10_temperature_p
+
+    if separation(cede_colat_s, cede_azi_s,
+                  theta, phi, sin_theta) <= cede_radius_s:
+        if separation(super_colat_s, super_azi_s,
+                      theta, phi, sin_theta) > super_radius_s:
+            local_vars[0] = cede_log10_temperature_s
+        elif super_log10_temperature_s >= 0.0:
+            local_vars[0] = super_log10_temperature_s
+    elif super_log10_temperature_s >= 0.0:
+        if separation(super_colat_s, super_azi_s,
+                      theta, phi, sin_theta) <= super_radius_s:
+            local_vars[0] = super_log10_temperature_s
 
     local_vars[1] = effectiveGravity(cos(theta),
                                      GEOM.R_eq,
