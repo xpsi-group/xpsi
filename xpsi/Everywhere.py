607a82f0987da72c19e549818457cfb43bf53d82
Tuomo Salmi <tuomosalmi1@gmail.com>
2024-05-21 11:12:29 +0200

    Fixing an additional phase-shift parameter always to zero for Everywhere.

--- xpsi/Everywhere.py
+++ xpsi/Everywhere.py
@@ -176,7 +176,14 @@ class Everywhere(ParameterSubspace):
         else: # let the custom subclass handle definitions; ignore bounds
             T = None
 
-        super(Everywhere, self).__init__(T, custom)
+        #An extra phase_shift parameter fixed to 0 just to be able use the standard
+        #Likelihood class when fitting data with Everywhere objects.
+        phase_shift = Parameter('phase_shift',
+                                strict_bounds = (-_np.infty, _np.infty),
+                                bounds = None,
+                                value = 0.0)
+
+        super(Everywhere, self).__init__(T, phase_shift, custom)
 
         self.time_invariant = time_invariant
         self._integrator_toggle = _integrator_toggle
