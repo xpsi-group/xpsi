957c53555cc0c2510eab9fe240510e007a739490
lmauviard <lucien@mauviard.fr>
2024-12-02 12:20:18 +0100

    Added fits reading ARF/RMF/RSP for Instrument class

--- xpsi/Instrument.py
+++ xpsi/Instrument.py
@@ -6,6 +6,9 @@ from xpsi.utils import make_verbose
 
 from xpsi.ParameterSubspace import ParameterSubspace
 
+from astropy.io import fits
+from astropy.table import Table
+
 class ResponseError(xpsiError):
     """ Raised if there is a problem with the input response matrix. """
 
@@ -308,3 +311,114 @@ class Instrument(ParameterSubspace):
         self._channels = channel_array
 
         yield
+
+    @classmethod
+    @make_verbose('Loading instrument response matrix',
+                  'Response matrix loaded')
+    def from_ogip_fits(cls,
+              RMF_path,
+              ARF_path=None,
+              min_channel=0,
+              max_channel=-1,
+              min_input=1,
+              max_input=-1,
+              datafolder=None,
+              **kwargs):
+        
+        """ Load any instrument response matrix. """
+
+        if datafolder:
+            ARF_path = _os.path.join( datafolder, ARF_path ) if ARF_path is not None else None
+            RMF_path = _os.path.join( datafolder, RMF_path )
+
+        # Open useful values in ARF/RMF/RSP
+        with fits.open( RMF_path ) as RMF_hdul:
+            RMF_header = RMF_hdul['MATRIX'].header
+        RMF_instr = RMF_header['INSTRUME'] 
+        DETCHANS = RMF_header['DETCHANS']
+        NUMGRP = RMF_header['NAXIS2']
+        TLMIN = RMF_header['TLMIN4']
+        TLMAX = RMF_header['TLMAX4']
+
+        # Handle the RSP case
+        if ARF_path is not None:
+            with fits.open( ARF_path ) as ARF_hdul:
+                assert RMF_instr == ARF_hdul['SPECRESP'].header['INSTRUME']
+
+        # Get the values and change the -1 values if requried
+        if max_channel == -1:
+            max_channel = DETCHANS -1
+        if max_input == -1:
+            max_input = NUMGRP
+        channels = _np.arange( min_channel, max_channel+1 )
+        inputs = _np.arange( min_input, max_input+1  )
+
+        # Perform routine checks
+        assert min_channel >= TLMIN and max_channel <= TLMAX
+        assert min_input >= 0 and max_input <= NUMGRP
+
+        # If everything in order, get the data
+        with fits.open( RMF_path ) as RMF_hdul:
+            RMF_MATRIX = RMF_hdul['MATRIX'].data
+            RMF_EBOUNDS = RMF_hdul['EBOUNDS'].data
+
+        # Get the channels from the data
+        RMF = _np.zeros((DETCHANS, NUMGRP))
+        for i, (N_GRP, F_CHAN, N_CHAN, RMF_line) in enumerate( zip(RMF_MATRIX['N_GRP'], RMF_MATRIX['F_CHAN'], RMF_MATRIX['N_CHAN'], RMF_MATRIX['MATRIX']) ):
+
+            # Skip if needed
+            if N_GRP == 0:
+                continue
+
+            # Check the values
+            if not isinstance(F_CHAN, _np.ndarray ):
+                F_CHAN = [F_CHAN]
+                N_CHAN = [N_CHAN]
+
+            # Add the values to the RMF
+            n_skip = 0 
+            for f_chan, n_chan in zip(F_CHAN,N_CHAN):
+
+                if n_chan == 0:
+                    continue
+
+                RMF[f_chan:f_chan+n_chan,i] += RMF_line[n_skip:n_skip+n_chan]
+                n_skip += n_chan
+
+        # Make the RSP, depending on the input files
+        if ARF_path is None:
+            RSP = RMF[min_channel:max_channel+1,min_input-1:max_input]
+            ARF_area = RMF.sum( axis=0 )
+        else:
+            ARF = Table.read(ARF_path, 'SPECRESP')
+            ARF_area = ARF['SPECRESP']
+            RSP = RMF * ARF_area
+            RSP = RSP[min_channel:max_channel+1,min_input-1:max_input]
+
+        # Find empty columns and lines
+        empty_channels = _np.all(RSP == 0, axis=1)
+        empty_inputs = _np.all(RSP == 0, axis=0)
+        RSP = RSP[~empty_channels][:,~empty_inputs]
+        channels = channels[ ~empty_channels ]
+        inputs = inputs[ ~empty_inputs ]
+        if empty_inputs.sum() > 0:
+            print(f'Triming the response matrix because it contains lines with only 0 values.\n Now min_input={inputs[0]} and max_input={inputs[-1]}')
+        if empty_channels.sum() > 0:
+            print(f'Triming the response matrix because it contains columns with only 0 values.\n Now min_channel={channels[0]} and max_channel={channels[-1]}')
+
+        # Get the edges of energies for both input and channel
+        energy_edges = _np.append( RMF_MATRIX['ENERG_LO'][inputs-1], RMF_MATRIX['ENERG_HI'][inputs[-1]-1]).astype(dtype=_np.double)
+        channel_energy_edges = _np.append(RMF_EBOUNDS['E_MIN'][channels],RMF_EBOUNDS['E_MAX'][channels[-1]])
+
+        # Make the instrument
+        Instrument = cls(RSP,
+                         energy_edges,
+                         channels,
+                         channel_energy_edges,
+                         **kwargs)
+        
+        # Add ARF and RMF for plotting
+        Instrument.RMF = RMF[min_channel:max_channel+1,min_input-1:max_input][~empty_channels][:,~empty_inputs]
+        Instrument.ARF = ARF_area[min_input-1:max_input][~empty_inputs]
+
+        return Instrument
