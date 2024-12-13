87ff07c37488192b2c3363e6f5977e4fa5f880a1
Tuomo Salmi <tuomo.salmi@helsinki.fi>
2024-12-13 16:16:20 +0200

    Importance Sampling tool example added and import paths generalized.

--- examples/examples_fast/Modules/main.py
+++ examples/examples_fast/Modules/main.py
@@ -6,29 +6,20 @@ import xpsi
 np.random.seed(xpsi._rank+10)
 
 import time
+import os
+import sys
 
-
+this_directory = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(this_directory)
 
 # Data
-if __name__ == '__main__':
-    from CustomInstrument import CustomInstrument
-    from CustomSignal import CustomSignal
-    from CustomPhotosphere import CustomPhotosphere
-    from CustomPrior import CustomPrior
-    data_path = "../Data/xpsi_good_realisation.dat"
-else:
-    from Modules.CustomInstrument import CustomInstrument
-    from Modules.CustomSignal import CustomSignal
-    from Modules.CustomPhotosphere import CustomPhotosphere
-    from Modules.CustomPrior import CustomPrior
+from CustomInstrument import CustomInstrument
+from CustomSignal import CustomSignal
+from CustomPhotosphere import CustomPhotosphere
+from CustomPrior import CustomPrior
 
-    data_path = "./Data/xpsi_good_realisation.dat"
-
-try:
-    data_loaded = np.loadtxt(data_path, dtype=np.double)
-except:
-    print("Loading the data assuming the notebook was run for documentation pages")
-    data_loaded = np.loadtxt('../../examples/examples_fast/Data/xpsi_good_realisation.dat', dtype=np.double)
+data_path = this_directory+"/../Data/xpsi_good_realisation.dat"
+data_loaded = np.loadtxt(data_path, dtype=np.double)
 
 data = xpsi.Data(data_loaded,
                      channels=np.arange(10,301),
