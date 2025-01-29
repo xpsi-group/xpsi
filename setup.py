0d7ccf5b77196fbb86d4f672df1b7a77e4d66ce0
Devarshi Choudhury <devarshic1408@gmail.com>
2025-01-29 12:08:19 +0100

    updating version numbers

--- setup.py
+++ setup.py
@@ -271,7 +271,7 @@ if __name__ == '__main__':
 
     setup(
         name = 'xpsi',
-        version = '3.0.2',
+        version = '3.0.3',
         author = 'The X-PSI Core Team',
         author_email = 'A.L.Watts@uva.nl',
         url = 'https://github.com/xpsi-group/xpsi',
