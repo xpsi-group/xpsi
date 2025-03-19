5c309fba1bceb8911f0dce27a83ac9ba62831f81
Tuomo Salmi <tuomo.salmi@helsinki.fi>
2025-03-19 14:22:43 +0200

    Version number change

--- setup.py
+++ setup.py
@@ -271,7 +271,7 @@ if __name__ == '__main__':
 
     setup(
         name = 'xpsi',
-        version = '3.0.5',
+        version = '3.0.6',
         author = 'The X-PSI Core Team',
         author_email = 'A.L.Watts@uva.nl',
         url = 'https://github.com/xpsi-group/xpsi',
