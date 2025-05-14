58c6eec5d4f69b6d3f37b9a2f336bb49f55c2f35
ckazantsev <ckazantsev@irap.omp.eu>
2025-05-14 14:12:43 +0200

    modification of plotting function

--- xpsi/Data.py
+++ xpsi/Data.py
@@ -591,31 +591,41 @@ class Data(object):
 
         # Do the plot
         mosaic = [['A','.'],['B','C']]
-        fig,axs = _mpl.pyplot.subplot_mosaic( mosaic , height_ratios=[1.,1.], width_ratios=[3,1], layout='constrained')
-
-        # Plot the pulse
-        ax1 = axs['A']
-        ax1.errorbar( x=phases, y=counts.sum(axis=0), yerr=_np.sqrt( counts.sum(axis=0) ), ds='steps-mid', color='black' )
-        ax1.set_ylabel('Counts')
+        fig,axs = _mpl.pyplot.subplot_mosaic( mosaic , height_ratios=[1.,1.], width_ratios=[3,1.5]) #, layout='constrained')
+        fig.subplots_adjust(wspace=0, hspace=0)
 
         # Plot the 2D data
         ax2 = axs['B']
         im = ax2.pcolormesh( phases, self.channels , counts, cmap=colormap)
-        ax2.sharex( ax1 )
         ax2.set_xlabel(r'Phase $\phi$ [cycles]')
         ax2.set_ylabel('PI channel')
         ax2.set_yscale('log')
+        xticks = ax2.get_xticks()
+        yticks = ax2.get_yticks()
+
+        # Plot the pulse
+        ax1 = axs['A']
+        ax1.sharex( ax2 )
+        ax1.tick_params(direction='in', which='both')
+        ax1.set_xticks(xticks) 
+        ax1.set_xticklabels([])
+
+        ax1.errorbar( x=phases, y=counts.sum(axis=0), yerr=_np.sqrt( counts.sum(axis=0) ), ds='steps-mid', color='black' )
+        ax1.set_ylabel('Counts')
         
         # Plot the spectrum
         ax3 = axs['C']
         ax3.sharey( ax2 )
-        ax3.get_yaxis().set_visible(False)
-        ax3.step( counts.sum(axis=1)/2, self.channels , color='black')
         ax3.set_yscale('log')
+        ax3.tick_params(direction='in', which='both')
+        ax3.set_yticks(yticks) 
+        ax3.set_yticklabels([])
+
+        ax3.step( counts.sum(axis=1)/2, self.channels , color='black')
         ax3.set_xlabel('Cts/chan.')
 
         # Add the colorbar    
-        fig.colorbar( im , ax=ax2 , label='Counts')
+        fig.colorbar( im , ax=ax3 , location='right',  label='Counts')
         fig.set_dpi(dpi)
 
         return fig, axs
