a2a92810b05a61ab8aea78765e86edc6a8dadc13
Devarshi Choudhury <devarshic1408@gmail.com>
2025-05-15 18:00:58 +0200

    further refinements

--- changelog.d/get_xpsi_version_for_changelog.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../xpsi")))
-
-from xpsi import __version__
-
-print(".".join(__version__.split(".")[:2]))
