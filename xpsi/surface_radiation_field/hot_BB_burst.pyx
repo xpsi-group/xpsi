5ad59fe06ff65b80de8e93418c85da1e9f341397
Bas Dorsman <50989936+Basdorsman@users.noreply.github.com>
2025-05-16 17:41:55 +0200

    69 add docstrings and explainer comments to hotpyx numericalpyx (#595)
    * docstrings and comments for num5d split
    
    * edited now also num4D interpolator
    
    * updates including cleaning the hot_num2D interpolators
    
    * updates for hot_BB
    
    * updated hot_BB_burst
    
    * Update hot_BB.pyx
    
    * Update hot_BB_burst.pyx
    
    * Update hot_Num2D.pyx
    
    * Update hot_Num4D.pyx
    
    Testing fix to CI failure
    
    * Update hot_Num4D.pyx
    
    2nd attempt at fix
    
    * fixing typos
    
    * added changelog stuff
    
    ---------
    
    Co-authored-by: Anna Watts <A.L.Watts@uva.nl>

--- xpsi/surface_radiation_field/hot_BB_burst.pyx
+++ xpsi/surface_radiation_field/hot_BB_burst.pyx
@@ -3,6 +3,20 @@
 #cython: nonecheck=False
 #cython: wraparound=False
 
+"""
+The BB atmosphere module, but modified for Bursts. Returns specific intensity, 
+which after normalisation should be J/cm^2/s/keV/steradian.
+
+Functions:
+    - init_hot_BB_burst: does nothing but must exist for placeholder purposes.
+    - free_hot_BB_burst: does nothing but must exist for placeholder purposes.
+    - eval_hot_BB_burst_I: evaluate the specific intensty.
+    - eval_hot_BB_burst_Q: evaluate stokes Q.
+    - eval_hot_BB_norm: normalize.
+
+"""
+
+
 from libc.math cimport exp, pow
 
 from xpsi.global_imports import _keV, _k_B
@@ -16,43 +30,36 @@ cdef double k_B = _k_B
 cdef double keV = _keV
 cdef double k_B_over_keV = k_B / keV
 
-#----------------------------------------------------------------------->>>
-# >>> User modifiable functions.
-# >>> Note that the user is entirely free to wrap thread-safe and
-# ... non-parallel external C routines from an external library.
-# >>> Thus the bodies of the following need not be written explicitly in
-# ... the Cython language.
-#----------------------------------------------------------------------->>>
 cdef void* init_hot_BB_burst(size_t numThreads, const _preloaded *const preloaded) noexcept nogil:
-    # This function must match the free management routine free_hot()
-    # in terms of freeing dynamically allocated memory. This is entirely
-    # the user's responsibility to manage.
-
-    # Return NULL if dynamic memory is not required for the model.
+    """
+    This function always returns NULL because no memory needs to be 
+    initialised, but must exist for placeholder purposes.
+    """
+    
     return NULL
 
 cdef int free_hot_BB_burst(size_t numThreads, void *const data) noexcept nogil:
-    # This function must match the initialisation routine init_hot()
-    # in terms of freeing dynamically allocated memory. This is entirely
-    # the user's responsibility to manage.
-    # The void pointer must be appropriately cast before memory is freed --
-    # only the user can know this at compile time.
-    # Just use free(<void*> data) iff no memory was dynamically
-    # allocated in the function:
-    #   init_local_hot()
-    # because data is expected to be NULL in this case
-
-    #printf("\nNo data to be freed.")
+    """
+    This function always returns SUCCESS = 0 because no memory needs to be 
+    freed, but must exist for placeholder purposes.
+    """
 
     return SUCCESS
 
 cdef double eval_hot_norm_BB_burst() noexcept nogil:
-    # Source radiation field normalisation which is independent of the
-    # parameters of the parametrised model -- i.e. cell properties, energy,
-    # and angle.
-    # Writing the normalisation here reduces the number of operations required
-    # during integration.
-    # The units of the specific intensity need to be J/cm^2/s/keV/steradian.
+    """
+    Source radiation field normalisation which is independent of the parameters
+    of the parametrised model -- i.e. cell properties, energy, and angle.
+    Writing the normalisation here reduces the number of operations required
+    during integration. The units of the specific intensity need to be 
+    J/cm^2/s/keV/steradian.
+    
+    Attributes:
+        noexcept nogil: Indicates that the function does not throw exceptions 
+                       and can be executed without the Global Interpreter Lock
+                       (GIL).
+    """
+
 
     return erg * Planck_dist_const
     
@@ -62,11 +69,26 @@ cdef double eval_hot_BB_burst_I(size_t THREAD,
                      double mu,
                      const double *const VEC,
                      void *const data) noexcept nogil:
-    # Arguments:
-    # E = photon energy in keV
-    # mu = cosine of ray zenith angle (i.e., angle to surface normal)
-    # VEC = variables such as temperature, effective gravity, ...
-    # data = numerical model data required for intensity evaluation
+    """
+    Evaluate the specific intensity of hot regions based on photon E and temp
+    and mu. Other parameters in VEC don't play role here.
+    
+    Arguments:
+        THREAD (size_t): Thread ID used for parallel execution.
+        E (double): Photon energy in electron rest energy for AMXPs.
+        mu (double): Cosine of the ray zenith angle (angle to surface normal).
+        VEC (const double *const): Pointer to variables (e.g., temperature, 
+                                                         effective gravity).
+        data (void *const): This will be pointing to NULL.
+        
+    Returns:
+        double: Calculated intensity value to be normalised.
+        
+    Attributes:
+        noexcept nogil: Indicates that the function does not throw exceptions 
+                       and can be executed without the Global Interpreter Lock 
+                       (GIL).
+    """
 
     cdef double temp = k_B_over_keV * pow(10.0, VEC[0])
 
@@ -78,11 +100,10 @@ cdef double eval_hot_BB_burst_Q(size_t THREAD,
                      double mu,
                      const double *const VEC,
                      void *const data) noexcept nogil:
-    # Arguments:
-    # E = photon energy in keV
-    # mu = cosine of ray zenith angle (i.e., angle to surface normal)
-    # VEC = variables such as temperature, effective gravity, ...
-    # data = numerical model data required for intensity evaluation
+    """
+    Computes stokes Q. Missing: why are these numbers correct?
+    
+    """
 
     cdef double I_E
     I_E = eval_hot_BB_burst_I(THREAD,E,mu,VEC,data)
