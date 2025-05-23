d5aa0d496b05715b07c4e45a50484255e4166087
Tuomo Salmi <t.h.j.salmi@uva.nl>
2025-05-23 11:08:45 +0300

    Version and changelog update (#607)
    * Changelog and version updates
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Anna Watts <A.L.Watts@uva.nl>

--- setup.py
+++ setup.py
@@ -231,7 +231,7 @@ if __name__ == '__main__':
 
     setup(
         name = 'xpsi',
-        version = '3.0.6',
+        version = '3.1.0',
         author = 'The X-PSI Core Team',
         author_email = 'A.L.Watts@uva.nl',
         url = 'https://github.com/xpsi-group/xpsi',
