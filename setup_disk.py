75f5efb22f52a34e665d1a9122bac1152caf2719
mao <mao@maodeMacBook-Air.local>
2025-10-22 22:35:53 +0200

    Setup file for compiling additional integrators

---
+++ setup_disk.py
@@ -0,0 +1,273 @@
+"""
+To compile to C code, and then compile the C code and link libraries:
+    --> CC=<path/to/compiler/executable> python setup.py install [--user]
+
+If compiling and linking with gcc, which is on $PATH:
+    --> CC=gcc python setup.py install [--user]
+
+If compiling and linking with Intel icc, with is on $PATH:
+    --> LDSHARED="icc -shared" CC=icc python setup.py install [--user]
+"""
+
+from setuptools import setup, Extension
+
+import argparse
+import sys
+
+desc = '''Options for installation'''
+parser = argparse.ArgumentParser(description=desc)
+
+try:
+    parser.add_argument('--noopenmp', help="Ignore the openmp install options", default=False, action="store_true")
+    if '--help' in sys.argv:
+        print(parser.print_help())
+        print('-----------------------------------------------------------------------------------')
+
+    # Setting the noopenmp option from argv
+    if '--noopenmp' in sys.argv:
+        noopenmp = True
+        sys.argv.remove("--noopenmp")
+    else:
+        noopenmp = False
+
+except:
+    pass
+
+if __name__ == '__main__':
+    import numpy
+    import sys
+    OS = sys.platform
+    import os
+    from os.path import join
+
+    if 'darwin' in OS or 'linux' in OS:
+        print('Operating system: ' + OS)
+
+        try:
+            import subprocess as sub
+        except ImportError:
+            print('The subprocess module is required to locate the GSL library.')
+            raise
+
+        try:
+            gsl_version = sub.check_output(['gsl-config','--version'])[:-1].decode("UTF-8")
+            gsl_prefix = sub.check_output(['gsl-config','--prefix'])[:-1].decode("UTF-8")
+        except Exception:
+            print('GNU Scientific Library cannot be located.')
+            raise
+        else:
+            print('GSL version: ' + gsl_version)
+            libraries = ['gsl','gslcblas','m'] # default BLAS interface for gsl
+            library_dirs = [gsl_prefix + '/lib']
+            _src_dir = os.path.dirname(os.path.abspath(__file__))
+            include_dirs = [gsl_prefix + '/include',
+                            numpy.get_include(),
+                            join(_src_dir, 'xpsi/include')]
+
+            # point to shared library at compile time so runtime resolution
+            # is not affected by environment variables, but is determined
+            # by the binary itself
+            extra_link_args = ['-Wl,-rpath,%s'%(str(gsl_prefix)+'/lib')]
+
+        try:
+            print("NOOPENMP =", noopenmp)
+            if not noopenmp :
+                if 'gcc' in os.environ['CC']:
+                    extra_compile_args=['-fopenmp',
+                                        '-march=native',
+                                        '-O3',
+                                        '-funroll-loops',
+                                        '-Wno-unused-function',
+                                        '-Wno-uninitialized',
+                                        '-Wno-cpp']
+                    extra_link_args.append('-fopenmp')
+                elif 'icc' in os.environ['CC']:
+                    # on high-performance systems using Intel processors
+                    # on compute nodes, it is usually recommended to select the
+                    # instruction set (extensions) optimised for a given processor
+                    extra_compile_args=['-qopenmp',
+                                        '-O3',
+                                        '-xHOST',
+                                        # alternative instruction set
+                                        '-axCORE-AVX2,AVX',
+                                        '-funroll-loops',
+                                        '-Wno-unused-function']
+                    extra_link_args.append('-qopenmp')
+                elif 'clang' in os.environ['CC']:
+                    extra_compile_args=['-fopenmp',
+                                        '-Wno-unused-function',
+                                        '-Wno-uninitialized',
+                                        '-Wno-#warnings',
+                                        '-Wno-error=format-security']
+                    extra_link_args.append('-fopenmp')
+                    # you might need these lookup paths for llvm clang on macOS
+                    # or you might need to edit these paths for your compiler
+                    #library_dirs.append('/usr/local/opt/llvm/lib')
+                    #include_dirs.append('/usr/local/opt/llvm/include')
+            else:
+                if 'gcc' in os.environ['CC']:
+                    extra_compile_args=['-march=native',
+                                        '-O3',
+                                        '-funroll-loops',
+                                        '-Wno-unused-function',
+                                        '-Wno-uninitialized',
+                                        '-Wno-cpp']
+                elif 'icc' in os.environ['CC']:
+                    # on high-performance systems using Intel processors
+                    # on compute nodes, it is usually recommended to select the
+                    # instruction set (extensions) optimised for a given processor
+                    extra_compile_args=['-O3',
+                                        '-xHOST',
+                                        # alternative instruction set
+                                        '-axCORE-AVX2,AVX',
+                                        '-funroll-loops',
+                                        '-Wno-unused-function']
+                elif 'clang' in os.environ['CC']:
+                    extra_compile_args=['-Wno-unused-function',
+                                        '-Wno-uninitialized',
+                                        '-Wno-#warnings',
+                                        '-Wno-error=format-security']
+                    # you might need these lookup paths for llvm clang on macOS
+                    # or you might need to edit these paths for your compiler
+                    #library_dirs.append('/usr/local/opt/llvm/lib')
+                    #include_dirs.append('/usr/local/opt/llvm/include')
+        except KeyError:
+            print('Export CC environment variable to "icc" or "gcc" or '
+                  '"clang", or modify the setup script for your compiler.')
+            raise
+    else:
+        print('Unsupported operating system. Manually inspect and modify '
+              'setup.py script.')
+        raise Exception
+
+    cmdclass = {}
+
+    try:
+        import Cython
+        print('Cython.__version__ == %s' % Cython.__version__)
+        from Cython.Distutils import build_ext
+    except ImportError:
+        print('Cannot use Cython. Trying to build extension from C files...')
+        try:
+            from distutils.command import build_ext
+        except ImportError:
+            print('Cannot import build_ext from distutils...')
+            raise
+        else:
+            cmdclass['build_ext'] = build_ext
+            file_extension = '.c'
+    else:
+        print('Using Cython to build extension from .pyx files...')
+        file_extension = '.pyx'
+        cmdclass['build_ext'] = build_ext
+
+    def EXTENSION(modname):
+
+        pathname = modname.replace('.', os.path.sep)
+
+        return Extension(modname,
+                         [pathname + file_extension],
+                         language = 'c',
+                         libraries = libraries,
+                         library_dirs = library_dirs,
+                         include_dirs = include_dirs,
+                         extra_compile_args = extra_compile_args,
+                         extra_link_args = extra_link_args)
+
+    modnames = ['xpsi.surface_radiation_field.effective_gravity_universal',
+                'xpsi.cellmesh.mesh_tools',
+                'xpsi.cellmesh.mesh',
+                'xpsi.cellmesh.polar_mesh',
+                'xpsi.cellmesh.global_mesh',
+                'xpsi.cellmesh.rays',
+                'xpsi.tools.energy_interpolator',
+                'xpsi.tools.energy_integrator',
+                'xpsi.tools.phase_integrator',
+                'xpsi.tools.phase_interpolator',
+                'xpsi.tools.synthesise',
+                'xpsi.tools.core',
+                'xpsi.likelihoods.default_background_marginalisation',
+                'xpsi.likelihoods._poisson_likelihood_given_background',
+                'xpsi.likelihoods._poisson_likelihood_given_background_IQU',
+                'xpsi.likelihoods._gaussian_likelihood_given_background_IQU',
+                'xpsi.likelihoods._gaussian_likelihood_QnUn',                
+                'xpsi.surface_radiation_field.core',
+                'xpsi.surface_radiation_field.preload',
+                'xpsi.surface_radiation_field.hot_user',
+                'xpsi.surface_radiation_field.hot_BB',
+                'xpsi.surface_radiation_field.hot_Num4D',
+                'xpsi.surface_radiation_field.hot_BB_burst',
+                'xpsi.surface_radiation_field.hot_Num2D',
+                'xpsi.surface_radiation_field.hot_Num2D_split',
+                'xpsi.surface_radiation_field.hot_Num5D_split',
+                'xpsi.surface_radiation_field.hot_wrapper',
+                'xpsi.surface_radiation_field.elsewhere_user',
+                'xpsi.surface_radiation_field.elsewhere_wrapper',
+                'xpsi.cellmesh.integrator',
+                'xpsi.cellmesh.integratorIQU',
+                'xpsi.cellmesh.integrator_for_azimuthal_invariance',
+                'xpsi.cellmesh.integrator_for_azimuthal_invariance_disk',
+                'xpsi.cellmesh.integrator_for_azimuthal_invariance_split',
+                'xpsi.cellmesh.integrator_for_azimuthal_invariance_split_disk',
+                'xpsi.cellmesh.integratorIQU_for_azimuthal_invariance',
+                'xpsi.cellmesh.integratorIQU_for_azimuthal_invariance_disk',
+                'xpsi.cellmesh.integratorIQU_for_azimuthal_invariance_split',
+                'xpsi.cellmesh.integratorIQU_for_azimuthal_invariance_split_disk',
+                'xpsi.cellmesh.integrator_for_time_invariance',
+                'xpsi.pixelmesh.METRIC_qK',
+                'xpsi.pixelmesh.RODES_qK',
+                'xpsi.pixelmesh.BOUNDARY_CONDITIONS',
+                'xpsi.pixelmesh.surfaceBisection',
+                'xpsi.pixelmesh.coordinateTransformation',
+                'xpsi.pixelmesh.RK_IP2S_tracer',
+                'xpsi.pixelmesh.get_IP_radius',
+                'xpsi.pixelmesh.globalRayMap',
+                'xpsi.surface_radiation_field.local_variables',
+                'xpsi.pixelmesh.integrator']
+    # modnames = ['xpsi.cellmesh.integrator_for_azimuthal_invariance_disk']
+    
+
+
+    extensions = []
+
+    for mod in modnames:
+        extensions.append(EXTENSION(mod))
+
+    for e in extensions:
+        e.cython_directives = {'language_level': "3"}
+
+    setup(
+        name = 'xpsi',
+        version = '3.1.0',
+        author = 'The X-PSI Core Team',
+        author_email = 'A.L.Watts@uva.nl',
+        url = 'https://github.com/xpsi-group/xpsi',
+        license = 'GPLv3',
+        description = """X-PSI: An open-source package for
+                         neutron star X-ray Pulse Simulation and Inference.""",
+        long_description = open('README.rst').read(),
+        packages = ['xpsi',
+                    'xpsi/PostProcessing',
+                    'xpsi/cellmesh',
+                    'xpsi/tools',
+                    'xpsi/surface_radiation_field',
+                    'xpsi/likelihoods',
+                    'xpsi/utilities',
+                    'xpsi/pixelmesh',
+                    'xpsi/include'],
+        install_requires = ['numpy'],
+        setup_requires = ['cython ~= 3.0.11'],
+        package_data = {'': ['README.rst', 'LICENSE']},
+        include_package_data = True,
+        ext_modules = extensions,
+        cmdclass = cmdclass,
+        classifiers = ['Development Status :: 5 - Production/Stable',
+                       'Intended Audience :: Science/Research',
+                       'Operating System :: Linux, macOS',
+                       'License :: OSI Approved :: GPLv3',
+                       'Programming Language :: Python'],
+        zip_safe = False,
+    )
+
+else:
+    pass
