fee131a50a15f342666a648974cdf34d168a7b57
YinghanMao <maoyinghan@126.com'git commit -m 'Testrun for two hotspot with disk occultation>
2025-11-21 18:22:14 +0800

    CustomsHotregions

--- examples/examples_modeling_tutorial/modules/CustomHotRegions.py
+++ examples/examples_modeling_tutorial/modules/CustomHotRegions.py
@@ -36,3 +36,39 @@ class CustomHotRegions_DiskOccultation(xpsi.HotRegions):
                                          R_in))
 
         return tuple(signals)
+
+    def integrate_stokes(self, st, energies, threads,
+                  hot_atmosphere_I, hot_atmosphere_Q, elsewhere_atmosphere, atm_ext_else, R_in = None):
+        """ Integrate Stokes parameters over the photospheric radiation field.
+
+        Calls the CellMesh integrator, with or without exploitation of
+        azimuthal invariance of the radiation field.
+
+        :param st: Instance of :class:`~.Spacetime.Spacetime`.
+
+        :param energies:
+            A one-dimensional :class:`numpy.ndarray` of energies in keV.
+
+        :param int threads:
+            Number of ``OpenMP`` threads for pulse integration.
+
+        """
+        if not isinstance(energies, tuple):
+            energies = [energies] * len(self)
+
+        signals = []
+        signalsQ = []
+        signalsU = []
+        for obj, E in zip(self._objects, energies):
+            sigs = obj.integrate_stokes(st, E,
+                                         threads,
+                                         hot_atmosphere_I,
+                                         hot_atmosphere_Q,
+                                         elsewhere_atmosphere,
+                                         atm_ext_else,
+                                         R_in)
+            signals.append(sigs[0])
+            signalsQ.append(sigs[1])
+            signalsU.append(sigs[2])
+
+        return tuple(signals), tuple(signalsQ), tuple(signalsU)
