db71419b60ec4eaf3053985a15d911a3c582a537
Bas Dorsman <superbas1996@gmail.com>
2025-11-26 12:00:31 +0100

    version bump to 3.2.0

--- setup.py
+++ setup.py
@@ -232,7 +232,7 @@ if __name__ == '__main__':
 
     setup(
         name = 'xpsi',
-        version = '3.1.0',
+        version = '3.2.0',
         author = 'The X-PSI Core Team',
         author_email = 'A.L.Watts@uva.nl',
         url = 'https://github.com/xpsi-group/xpsi',
