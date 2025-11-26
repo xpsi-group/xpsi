7eedfab377aa797c81af8688cd45a6131ddfdee2
Bas Dorsman <superbas1996@gmail.com>
2025-11-26 10:25:53 +0100

    R_in now outside stokes if statement in customphotosphere and removed customhotregions which was unused

--- examples/examples_modeling_tutorial/modules/CustomPhotosphere.py
+++ examples/examples_modeling_tutorial/modules/CustomPhotosphere.py
@@ -197,10 +197,8 @@ class CustomPhotosphere_NumA5(xpsi.Photosphere):
                 except:
                     else_atm_ext = None
 
+                R_in = self.disk['R_in'] * 1000
                 if self._stokes:
-                    R_in = self.disk['R_in'] * 1000
-                    # print(R_in)
-                    # R_in = 24.5 * 1000 # if want a constant R_in
                     if self._disk_blocking:
                         self._signal, self._signalQ, self._signalU  = self._hot.integrate_stokes(self._spacetime,
                                                     energies,
@@ -235,8 +233,6 @@ class CustomPhotosphere_NumA5(xpsi.Photosphere):
                     self._signalQ = tuple(map(tuple, tempQ))
                     self._signalU = tuple(map(tuple, tempU))
                 else:
-                    R_in = self.disk['R_in'] * 1000
-                    # print(R_in)
                     if self._disk_blocking:
                         self._signal = self._hot.integrate(self._spacetime,
                                                     energies,
